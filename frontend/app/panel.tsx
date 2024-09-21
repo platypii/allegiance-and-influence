fcf06a357811cf2bfc8863e50035b746a1d545fd
Kenny Daniel <platypii@gmail.com>
2024-09-21 16:46:28 -0700

    Chat style

--- frontend/app/panel.tsx
+++ frontend/app/panel.tsx
@@ -9,7 +9,10 @@ export default function Panel({ chatWith }: PanelProps) {
     <div className={styles.panel} style={chatWith ? {} : {width: "0px"}}>
       <div className={styles.panelContent}>
         <h1>{chatWith}</h1>
-        <div className={styles.chatArea} />
+        <div className={styles.chatArea}>
+          <div className={styles.user}>Hello</div>
+          <div className={styles.assistant}>Hi</div>
+        </div>
         <div className={styles.inputArea}>
           <input type="text" placeholder="Make an argument" />
           <button>Supply</button>
