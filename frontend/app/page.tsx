a2537b827c67cc3e0668e7a9afb9d59236f01f44
Kenny Daniel <platypii@gmail.com>
2024-09-21 12:19:29 -0700

    Add react-force-graph

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import ForceGraph from "react-force-graph-2d"
 import styles from "./page.module.css"
 
 export default function Home() {
@@ -8,6 +11,22 @@ export default function Home() {
           <h1>How To Win Friends and Influence Agents</h1>
           A game of human-machine influence
         </div>
+        <ForceGraph
+          graphData={{
+            nodes: [
+              { id: "You", color: "blue" },
+              { id: "Agent", color: "red" },
+            ],
+            links: [{ source: "You", target: "Agent" }],
+          }}
+          nodeCanvasObject={(node, ctx, globalScale) => {
+            const label = node.id
+            const fontSize = 12 / globalScale
+            ctx.font = `${fontSize}px Sans-Serif`
+            ctx.fillStyle = node.color
+            ctx.fillText(label, node.x, node.y)
+          }}
+        />
       </main>
     </div>
   )
